441cfcc46bc3f8d84f8be1ff6307a41a6da6c6f0
Aditya Maheshwari <aditya.maheshwari@einfochips.com>
2026-02-19 13:41:35 +0530

    refactor: optimize state management and implement Log Component(19-02-2026)

--- Tic-Tac-Toe/src/App.jsx
+++ Tic-Tac-Toe/src/App.jsx
@@ -1,24 +1,52 @@
 import { useState } from "react";
 import Player from "./components/Player.jsx";
 import GameBoard from "./components/GameBoard.jsx";
+import Log from "./components/Log.jsx";
 
 function App() {
+  const [gameTurns, setGameTurns] = useState([]); //whenever square is selected we have to add new turn to this array
   const [activePlayer, setActivePlayer] =useState('X');
 
-  function handleSelectSquare(){
+  function handleSelectSquare(rowIndex, colIndex){ 
     setActivePlayer((curActivePlayer) => curActivePlayer === 'X' ? 'O' : 'X');
+    setGameTurns((prevTurns) => {
+      let currentPlayer = 'X';
+
+      if(prevTurns.length > 0 && prevTurns[0].player === 'X'){
+        currentPlayer = 'O';
+      }
+
+      const updatedTurns = [
+        {square : { row : rowIndex, col: colIndex}, player : currentPlayer },
+        ...prevTurns,
+      ];
+      return updatedTurns;
+    })
   }
   return (
     <main>
       <div id = "game-container">
         <ol id="players" className="highlight-player">
-          <Player intialName = "Player 1" symbol = "X" isActive={activePlayer === 'X'}/>
-          <Player intialName = "Player 2" symbol = "O" isActive={activePlayer === 'O'}/>
+          <Player
+            intialName = "Player 1" 
+            symbol = "X" 
+            isActive={activePlayer === 'X'}
+          />
+          <Player
+            intialName = "Player 2"
+            symbol = "O" 
+            isActive={activePlayer === 'O'}
+          />
         </ol>
-        <GameBoard onSelectSquare={handleSelectSquare} activePlayerSymbol={activePlayer}/>
+        <GameBoard 
+          onSelectSquare={handleSelectSquare}
+          turns = {gameTurns} 
+        />
         
-
       </div>
+      <Log 
+      turns = {gameTurns}
+      />
     </main>
   )
 }
