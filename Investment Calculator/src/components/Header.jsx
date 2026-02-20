54b0282b75bdbcc1f62c63c9e337e4a020464f0e
Aditya Maheshwari <aditya.maheshwari@einfochips.com>
2026-02-20 18:13:52 +0530

    Create Investment Calculator(20-02-2026)

---
+++ Investment Calculator/src/components/Header.jsx
@@ -0,0 +1,14 @@
+import logo from '../assets/investment-calculator-logo.png';
+export default function Header() {
+    return(
+        <>
+        <header id="header">
+            <img src={logo} alt="Investment-Calculator-logo-Image"/>
+            <h1>Investment Calculator</h1>
+        </header>
+        
+        </>
+    
+    );
+
+}
---
+++ Investment Calculator/src/components/UserInput.jsx
@@ -0,0 +1,51 @@
+export default function UserInput({onChange,userInput}){
+
+    return(
+        <section id = "user-input">
+            <div className="input-group">
+                <p>
+                    <label id="user-input label">Initial Investment</label>
+                    <input 
+                        id="user-input input"
+                        type="number" 
+                        required
+                        value={userInput.initialInvestment}
+                        onChange={(event) => onChange('initialInvestment',event.target.value)}
+                    />
+                </p>
+                <p>
+                    <label id="user-input label"> Annual Investment</label>
+                    <input 
+                        id="user-input input"
+                        type="number" 
+                        required
+                        value={userInput.annualInvestment}
+                        onChange={(event) => onChange('annualInvestment',event.target.value)}
+                    />
+                </p>
+            </div>
+            <div className="input-group">
+                <p>
+                    <label id="user-input label">Expected Return</label>
+                    <input 
+                        id="user-input input"
+                        type="number" 
+                        required
+                        value={userInput.expectedReturn}
+                        onChange={(event) => onChange('expectedReturn',event.target.value)}
+                    />
+                </p>
+                <p>
+                    <label id="user-input label">Duration</label>
+                    <input 
+                        id="user-input input"
+                        type="number" 
+                        required
+                        value={userInput.duration}
+                        onChange={(event) => onChange('duration',event.target.value)}
+                    />
+                </p>
+            </div>
+        </section>
+    );
+}
